7c599963f78885f87a347eac95576f4334274c0d
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-10-10 18:26:37 -0400

    set up initial connection for mongodb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+mongoose.connect('mongodb://localhost:27017/database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
+
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 export default App;
