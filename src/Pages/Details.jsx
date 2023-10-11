3cdc2729a6026925a98a657ad35d30afbf44a485
JakeSifo <bearcoco5@gmail.com>
2023-10-11 15:35:23 -0400

    Tweaked dummy data

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -3,30 +3,30 @@
 import React from 'react';
 import { Users } from '..';
 
-const EmployeeDetails = ({ Users }) => {
+const EmployeeDetails = () => {
     return (
         <div>
             <h2>Employee Details</h2>
             <div>
-                <h3>Name:</h3> {Users.name}
+                <h3>Name:</h3> {Users[0].name}
             </div>
             <div>
-                <h3>Role:</h3> {Users.role}
+                <h3>Role:</h3> {Users[0].role}
             </div>
             <div>
-                <h3>Phone Number:</h3> {Users.phone}
+                <h3>Phone Number:</h3> {Users[0].phone}
             </div>
             <div>
-                <h3>Location:</h3> {Users.location}
+                <h3>Location:</h3> {Users[0].location}
             </div>
             <div>
-                <h3>Email:</h3> {Users.email}
+                <h3>Email:</h3> {Users[0].email}
             </div>
             <div>
-                <h3>Manager:</h3> {Users.manager}
+                <h3>Manager:</h3> {Users[0].manager}
             </div>
             <div>
-                <h3>Salary:</h3> {Users.salary}
+                <h3>Salary:</h3> {Users[0].salary}
             </div>
         </div>
     );
