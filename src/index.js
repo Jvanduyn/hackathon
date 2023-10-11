5ce56a39e2f13421d41bc57f398ed4da14177edf
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-10-11 11:18:31 -0400

    set up faker data function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
+import { faker } from '@faker-js/faker';
+import { options } from './routes/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +11,17 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const { faker } = require('@faker-js/faker');
+
+export function createRandomUser(): User {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    name: faker.person.fullName(),
+    role: faker.person.jobTitle(),
+    phone: faker.phone.number(),
+    location: faker.location.state(),
+    salary: faker.finance.amount(45000, 200000, 2, '$')
+  }
+};
