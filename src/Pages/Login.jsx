f53e3db6c6c64773495b59855d21c9099e6646a2
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-10-11 13:47:25 -0400

    login page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+
+import Auth from "../utils/Auth";
+
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: "", password: "" });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.login.token, data.login.user._id);
+        } catch (e) {
+            console.error(e);
+        }
+
+        // clear form values
+        setFormState({
+            email: "",
+            password: "",
+        });
+    };
