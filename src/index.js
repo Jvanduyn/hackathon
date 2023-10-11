0502bc2dc5e767bf1495805039a4f33c941d0639
JakeSifo <bearcoco5@gmail.com>
2023-10-11 13:43:16 -0400

    Added managers

--- src/index.js
+++ src/index.js
@@ -11,11 +11,26 @@ root.render(
   </React.StrictMode>
 );
 
-export function createRandomUser() {
+const numManagers = 5;
+const managers = [];
+for (let x = 0; x < numManagers; x++) {
+  let first = faker.person.firstName();
+  let last = faker.person.lastName();
+  managers[x] = {
+    password: faker.internet.password(),
+    name: first + ' ' + last,
+    role: 'Manager',
+    phone: faker.phone.number(),
+    location: faker.location.state(),
+    salary: faker.finance.amount(45000, 200000, 2, '$'),
+    email: faker.internet.email({ firstName: first, lastName: last }),
+  }
+}
+function createRandomUser() {
   let isHR = Math.random() > 0.9;
   let first = faker.person.firstName();
   let last = faker.person.lastName();
-  
+
   return {
     password: faker.internet.password(),
     name: first + ' ' + last,
@@ -23,11 +38,13 @@ export function createRandomUser() {
     phone: faker.phone.number(),
     location: faker.location.state(),
     salary: faker.finance.amount(45000, 200000, 2, '$'),
-    email: faker.internet.email({firstName: first, lastName: last}),
+    email: faker.internet.email({ firstName: first, lastName: last }),
+    manager: managers[faker.number.int(4)].email,
   }
 };
 
 export const Users = faker.helpers.multiple(createRandomUser, {
   count: 1000,
 });
+Users.push(...managers);
 console.log(Users);
