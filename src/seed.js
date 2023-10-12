5f0e514b2b5ed3a8091ac0715f67d3368f5abdec
Jake V <jakev@jakev.com>
2023-10-12 16:50:23 -0400

    updating jobs

--- src/seed.js
+++ src/seed.js
@@ -48,11 +48,17 @@ function createRandomUser() {
     let isHR = Math.random() > 0.9;
     let first = faker.person.firstName();
     let last = faker.person.lastName();
-
+    let jobs = {
+        'Developer':1.5, 'Designer':1, 'Facilitator':0.8, 'Director':1.5, 'Representative':0.7,
+        'Technician':0.7, 'Assistant':0.5, 'Consultant':0.85, 'Executive':1.7, 'Associate':1.3, 'Architect':1.1,
+        'Specialist':0.9, 'Engineer':1.1, 'Producer':0.85, 'Liaison':1.1, 'Analyst':1.1, 'Supervisor':1.1,
+        'Officer':1.6, 'Strategist':1.12, 'HR':1.1, 'Orchestrator':0.9, 'Planner':0.6, 'Agent':0.7, 'Manager':1.1,
+        'Administrator':0.95, 'Coordinator':0.65}
+    let job = Object.keys(jobs)[Math.floor(math.random()*jobs.length)]
     return {
         password: faker.internet.password(),
         name: first + ' ' + last,
-        role: isHR ? 'HR' : faker.person.jobTitle(),
+        role: isHR ? 'HR' : jobs.keys[faker.number.int(jobs.length)],
         phone: faker.phone.number(),
         location: faker.location.state(),
         salary: parseFloat(faker.finance.amount(45000, 200000, 2)),
@@ -70,17 +76,17 @@ Users.push(...managers);
 // Clear and insert new data
 const clearAndInsertUsers = async (usersData) => {
     try {
-      await collection.deleteMany({});
-      console.log('Deleted existing users.');
-  
-      const users = await collection.insertMany(usersData);
-      console.log('Users inserted successfully:', users);
-      client.close();
-    } catch (error) {
-      console.error('Error:', error);
-      client.close();
-    }
-  };
+        await collection.deleteMany({});
+        console.log('Deleted existing users.');
 
-  
-  clearAndInsertUsers(Users);
+        const users = await collection.insertMany(usersData);
+        console.log('Users inserted successfully:', users);
+        client.close();
+    } catch (error) {
+        console.error('Error:', error);
+        client.close();
+    }
+};
+
+
+clearAndInsertUsers(Users);
