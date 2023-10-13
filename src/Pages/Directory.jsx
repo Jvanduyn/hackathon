6417da6c760a5198cede139349b8a121a7360fbb
JakeSifo <bearcoco5@gmail.com>
2023-10-12 21:24:09 -0400

    Added search box

--- src/Pages/Directory.jsx
+++ src/Pages/Directory.jsx
@@ -1,60 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  style: 'currency',
+  currency: 'USD',
 });
 
 const EmployeeDirectory = ({ users }) => {
-    const [visibleUsers, setVisibleUsers] = useState(50);
-    const [inputText, setInputText] = useState('');
-    const [prediction, setPrediction] = useState('');
-    const navigate = useNavigate();
+  const [visibleUsers, setVisibleUsers] = useState(50);
+  const [inputText, setInputText] = useState('');
+  const [predictionText, setPredictionText] = useState('');
+  const [prediction, setPrediction] = useState('');
+  const navigate = useNavigate();
 
-    const handleLoadMore = () => {
-        setVisibleUsers((prevVisibleUsers) => prevVisibleUsers + 50);
-    };
-    const handleLogout = () => {
-        localStorage.removeItem('directoryUser');
-        navigate('/login');
-    };
-    const handlePredict = () => {
-        // Use the 'inputText' value in your prediction function
-        console.log('Predicting salary for:', inputText);
-        // Call your prediction function with 'inputText'
-        // predictionFunction(inputText);
-        setPrediction(inputText);
-    };
+  const handleLoadMore = () => {
+    setVisibleUsers((prevVisibleUsers) => prevVisibleUsers + 50);
+  };
 
-    return (
-        <div>
-            <button onClick={handleLogout}>Logout</button>
-            <div>
-                <input
-                    type="text"
-                    value={inputText}
-                    onChange={(e) => setInputText(e.target.value)}
-                    placeholder="Enter a role"
-                />
-                <button onClick={handlePredict}>Salary Prediction</button>
-            </div>
-            <div>
-                <p>{prediction ? formatter.format(prediction) : ''}</p>
-            </div>
-            <h2>Employee Directory</h2>
-            <ul>
-                {users.slice(0, visibleUsers).map((employee, index) => (
-                    <li key={index}>
-                        <Link to={`/details?i=${index}`}>{employee.name}</Link>
-                    </li>
-                ))}
-            </ul>
-            {visibleUsers < users.length && (
-                <button onClick={handleLoadMore}>Load More</button>
-            )}
-        </div>
-    );
+  const handleLogout = () => {
+    localStorage.removeItem('directoryUser');
+    navigate('/login');
+  };
+
+  const handlePredict = () => {
+    console.log('Predicting salary for:', predictionText);
+    setPrediction(predictionText);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(inputText.toLowerCase())
+  );
+
+  return (
+    <div>
+      <button onClick={handleLogout}>Logout</button>
+      <div>
+        <input
+          type="text"
+          value={predictionText}
+          onChange={(e) => setPredictionText(e.target.value)}
+          placeholder="Search by name"
+        />
+        <button onClick={handlePredict}>Salary Prediction</button>
+      </div>
+      <div>
+        <p>{prediction ? formatter.format(prediction) : ''}</p>
+      </div>
+      <h2>Employee Directory</h2>
+      <input
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="Search by name"
+        />
+      <ul>
+        {filteredUsers.slice(0, visibleUsers).map((employee, index) => (
+          <li key={index}>
+            <Link to={`/details?i=${users.indexOf(employee)}`}>{employee.name}</Link>
+          </li>
+        ))}
+      </ul>
+      {visibleUsers < filteredUsers.length && (
+        <button onClick={handleLoadMore}>Load More</button>
+      )}
+    </div>
+  );
 };
 
 export default EmployeeDirectory;
